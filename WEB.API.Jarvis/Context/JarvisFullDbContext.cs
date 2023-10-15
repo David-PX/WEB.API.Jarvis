d2ac2cb20505f107b373c5b760e88a5bcee721f9
Alex Valenzuela <alexvalenzuela0108@gmail.com>
2023-10-15 19:18:16 -0400

    api

--- WEB.API.Jarvis/Context/JarvisFullDbContext.cs
+++ WEB.API.Jarvis/Context/JarvisFullDbContext.cs
@@ -20,6 +20,18 @@ public partial class JarvisFullDbContext : DbContext
 
     public virtual DbSet<AcademicStatus> AcademicStatuses { get; set; }
 
+    public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
+
+    public virtual DbSet<AspNetRoleClaim> AspNetRoleClaims { get; set; }
+
+    public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
+
+    public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
+
+    public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
+
+    public virtual DbSet<AspNetUserToken> AspNetUserTokens { get; set; }
+
     public virtual DbSet<Attendance> Attendances { get; set; }
 
     public virtual DbSet<AttendanceStudent> AttendanceStudents { get; set; }
@@ -63,6 +75,10 @@ public partial class JarvisFullDbContext : DbContext
     public virtual DbSet<InvoicesStatus> InvoicesStatuses { get; set; }
 
     public virtual DbSet<Payment> Payments { get; set; }
+
+    public virtual DbSet<Pensum> Pensums { get; set; }
+
+    public virtual DbSet<PensumSubject> PensumSubjects { get; set; }
 
     public virtual DbSet<Report> Reports { get; set; }
 
@@ -134,6 +150,72 @@ public partial class JarvisFullDbContext : DbContext
             entity.Property(e => e.DeletedDate).HasColumnType("datetime");
             entity.Property(e => e.UpdatedBy).HasMaxLength(450);
             entity.Property(e => e.UpdatedDate).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<AspNetRole>(entity =>
+        {
+            entity.HasIndex(e => e.NormalizedName, "RoleNameIndex")
+                .IsUnique()
+                .HasFilter("([NormalizedName] IS NOT NULL)");
+
+            entity.Property(e => e.Name).HasMaxLength(256);
+            entity.Property(e => e.NormalizedName).HasMaxLength(256);
+        });
+
+        modelBuilder.Entity<AspNetRoleClaim>(entity =>
+        {
+            entity.HasIndex(e => e.RoleId, "IX_AspNetRoleClaims_RoleId");
+
+            entity.HasOne(d => d.Role).WithMany(p => p.AspNetRoleClaims).HasForeignKey(d => d.RoleId);
+        });
+
+        modelBuilder.Entity<AspNetUser>(entity =>
+        {
+            entity.HasIndex(e => e.NormalizedEmail, "EmailIndex");
+
+            entity.HasIndex(e => e.NormalizedUserName, "UserNameIndex")
+                .IsUnique()
+                .HasFilter("([NormalizedUserName] IS NOT NULL)");
+
+            entity.Property(e => e.Email).HasMaxLength(256);
+            entity.Property(e => e.NormalizedEmail).HasMaxLength(256);
+            entity.Property(e => e.NormalizedUserName).HasMaxLength(256);
+            entity.Property(e => e.UserName).HasMaxLength(256);
+
+            entity.HasMany(d => d.Roles).WithMany(p => p.Users)
+                .UsingEntity<Dictionary<string, object>>(
+                    "AspNetUserRole",
+                    r => r.HasOne<AspNetRole>().WithMany().HasForeignKey("RoleId"),
+                    l => l.HasOne<AspNetUser>().WithMany().HasForeignKey("UserId"),
+                    j =>
+                    {
+                        j.HasKey("UserId", "RoleId");
+                        j.ToTable("AspNetUserRoles");
+                        j.HasIndex(new[] { "RoleId" }, "IX_AspNetUserRoles_RoleId");
+                    });
+        });
+
+        modelBuilder.Entity<AspNetUserClaim>(entity =>
+        {
+            entity.HasIndex(e => e.UserId, "IX_AspNetUserClaims_UserId");
+
+            entity.HasOne(d => d.User).WithMany(p => p.AspNetUserClaims).HasForeignKey(d => d.UserId);
+        });
+
+        modelBuilder.Entity<AspNetUserLogin>(entity =>
+        {
+            entity.HasKey(e => new { e.LoginProvider, e.ProviderKey });
+
+            entity.HasIndex(e => e.UserId, "IX_AspNetUserLogins_UserId");
+
+            entity.HasOne(d => d.User).WithMany(p => p.AspNetUserLogins).HasForeignKey(d => d.UserId);
+        });
+
+        modelBuilder.Entity<AspNetUserToken>(entity =>
+        {
+            entity.HasKey(e => new { e.UserId, e.LoginProvider, e.Name });
+
+            entity.HasOne(d => d.User).WithMany(p => p.AspNetUserTokens).HasForeignKey(d => d.UserId);
         });
 
         modelBuilder.Entity<Attendance>(entity =>
@@ -458,7 +540,9 @@ public partial class JarvisFullDbContext : DbContext
             entity.Property(e => e.CreatedDate).HasColumnType("datetime");
             entity.Property(e => e.DeletedBy).HasMaxLength(450);
             entity.Property(e => e.DeletedDate).HasColumnType("datetime");
-            entity.Property(e => e.Email).HasMaxLength(150);
+            entity.Property(e => e.Email)
+                .HasMaxLength(150)
+                .IsUnicode(false);
             entity.Property(e => e.Gender)
                 .HasMaxLength(30)
                 .IsUnicode(false);
@@ -696,6 +780,40 @@ public partial class JarvisFullDbContext : DbContext
             entity.HasOne(d => d.Invoice).WithMany(p => p.Payments)
                 .HasForeignKey(d => d.InvoiceId)
                 .HasConstraintName("FK_Payments_InvoiceID");
+        });
+
+        modelBuilder.Entity<Pensum>(entity =>
+        {
+            entity.HasKey(e => e.PensumId).HasName("PK__Pensum__225B95C2F34A6CF1");
+
+            entity.ToTable("Pensum");
+
+            entity.Property(e => e.PensumId)
+                .ValueGeneratedNever()
+                .HasColumnName("PensumID");
+            entity.Property(e => e.CarrerId).HasColumnName("CarrerID");
+            entity.Property(e => e.PensumName)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Carrer).WithMany(p => p.Pensums)
+                .HasForeignKey(d => d.CarrerId)
+                .HasConstraintName("FK_Pensum.CarrerID");
+        });
+
+        modelBuilder.Entity<PensumSubject>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.Property(e => e.PensumId).HasColumnName("PensumID");
+            entity.Property(e => e.SubjectId)
+                .HasMaxLength(7)
+                .IsUnicode(false)
+                .HasColumnName("SubjectID");
+
+            entity.HasOne(d => d.Pensum).WithMany()
+                .HasForeignKey(d => d.PensumId)
+                .HasConstraintName("FK_PensumSubjects.PensumID");
         });
 
         modelBuilder.Entity<Report>(entity =>
---
+++ WEB.API.Jarvis/Models/Pensum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jarvis.WEB.API.Models;
+
+public partial class Pensum
+{
+    public Guid PensumId { get; set; }
+
+    public string? PensumName { get; set; }
+
+    public int? Version { get; set; }
+
+    public int? TotalCredits { get; set; }
+
+    public int? TotalTrimester { get; set; }
+
+    public Guid? CarrerId { get; set; }
+
+    public virtual Career? Carrer { get; set; }
+}
