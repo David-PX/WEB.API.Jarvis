d67cbedcdf68860e092b1dd17c6ce211adc74705
Alex Valenzuela <alexvalenzuela0108@gmail.com>
2023-10-16 23:30:33 -0400

    Pruea

--- WEB.API.Jarvis/Controllers/SectionStudentsController.cs
+++ WEB.API.Jarvis/Controllers/SectionStudentsController.cs
@@ -6,55 +6,53 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Jarvis.WEB.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/sectionstudents")]
     [ApiController]
     public class SectionStudentsController : ControllerBase
     {
-        [Route("api/sectionstudents")]
-        [ApiController]
-        public class SectionStudentController : ControllerBase
+
+        private readonly JarvisFullDbContext _context;
+
+        public SectionStudentsController(JarvisFullDbContext context)
         {
-            private readonly JarvisFullDbContext _context;
-
-            public SectionStudentController(JarvisFullDbContext context)
-            {
-                _context = context;
-            }
-
-            // GET api/sectionstudents/{studentId}
-            [HttpGet("{studentId}")]
-            public async Task<IActionResult> GetSectionStudentsByStudentId(string studentId)
-            {
-                var sectionStudents = await _context.SectionStudents
-                    .Include(ss => ss.Section)
-                    .Where(ss => ss.StudentId == studentId)
-                    .ToListAsync();
-
-                return Ok(sectionStudents);
-            }
-
-            // POST api/sectionstudents
-            [HttpPost]
-            public async Task<IActionResult> CreateSectionStudent(SectionStudent sectionStudent)
-            {
-                if (sectionStudent == null)
-                {
-                    return BadRequest("Datos no válidos");
-                }
-
-                try
-                {
-                    _context.SectionStudents.Add(sectionStudent);
-                    await _context.SaveChangesAsync();
-
-                    return CreatedAtAction("GetSectionStudentsByStudentId", new { studentId = sectionStudent.StudentId }, sectionStudent);
-                }
-                catch (Exception ex)
-                {
-                    return StatusCode(500, $"Error interno del servidor: {ex.Message}");
-                }
-            }
-
+            _context = context;
         }
+
+        // GET api/sectionstudents/{studentId}
+        [HttpGet("{studentId}")]
+        public async Task<IActionResult> GetSectionStudentsByStudentId(string studentId)
+        {
+            var sectionStudents = await _context.SectionStudents
+                .Include(ss => ss.Section)
+                .Where(ss => ss.StudentId == studentId)
+                .ToListAsync();
+
+
+            return StatusCode(StatusCodes.Status200OK, sectionStudents);
+        }
+
+        // POST api/sectionstudents
+        [HttpPost]
+        public async Task<IActionResult> CreateSectionStudent(SectionStudent sectionStudent)
+        {
+            if (sectionStudent == null)
+            {
+                return BadRequest("Datos no válidos");
+            }
+
+            try
+            {
+                _context.SectionStudents.Add(sectionStudent);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction("GetSectionStudentsByStudentId", new { studentId = sectionStudent.StudentId }, sectionStudent);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error interno del servidor: {ex.Message}");
+            }
+        }
+
     }
+
 }
